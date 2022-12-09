8acbe95d6a4de127dc88fc4f88540e77ca401013
octopunkk <anais.derue@insa-rennes.fr>
2022-12-09 15:11:00 +0100

    refacto Day 9

--- Day9/part1-2.js
+++ Day9/part1-2.js
@@ -2,19 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync("input.txt", { encoding: "utf8" }).split("\r\n");
 
 const ropeSize = 10;
-let rope = [
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0],
-];
-
+let rope = new Array(ropeSize).fill(null).map(() => [0, 0]);
 let tailUniquePositions = new Set();
 
 const moveTail = (head, tail) => {
@@ -34,7 +22,7 @@ const moveTail = (head, tail) => {
     tail[0] += dist[0] - Math.sign(dist[0]);
     tail[1] += dist[1] - Math.sign(dist[1]);
   }
-  return [...tail];
+  return tail;
 };
 
 const moveHead = (head, dir) => {
@@ -42,27 +30,15 @@ const moveHead = (head, dir) => {
   if (dir === "L") head[0] -= 1;
   if (dir === "U") head[1] -= 1;
   if (dir === "D") head[1] += 1;
-  return [...head];
-};
-
-const displayRope = (canvasSize, rope) => {
-  for (let i = 0; i < canvasSize; i++) {
-    let string = "";
-    for (let j = 0; j < canvasSize; j++) {
-      let knotFound = rope.findIndex((knot) => knot[0] === j && knot[1] === i);
-      if (knotFound >= 0) string += knotFound;
-      else string += ".";
-    }
-    console.log(string);
-  }
+  return head;
 };
 
 input.forEach((move) => {
   move = move.split(" ");
   for (let i = 0; i < Number(move[1]); i++) {
-    rope[0] = [...moveHead(rope[0], move[0])];
+    rope[0] = moveHead(rope[0], move[0]);
     for (let knot = 1; knot < ropeSize; knot++) {
-      rope[knot] = [...moveTail(rope[knot - 1], rope[knot])];
+      rope[knot] = moveTail(rope[knot - 1], rope[knot]);
     }
     tailUniquePositions.add(
       `${rope[ropeSize - 1][0]}, ${rope[ropeSize - 1][1]}`
