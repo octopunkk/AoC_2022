fe3e71d0d34a0937ccadc8a85334fc9f7a07e88b
octopunkk <anais.derue@insa-rennes.fr>
2022-12-14 10:34:46 +0100

    add Day 14

---
+++ Day14/part1.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", { encoding: "utf8" }).split("\r\n");
+
+let cave = [];
+let bottomRock = -Infinity;
+input.forEach((element) => {
+  let regex = /(\d+)/g;
+  let match = element.match(regex);
+  let rockLines = [];
+  for (let i = 0; i < match.length; i += 2) {
+    rockLines.push([Number(match[i + 1]), Number(match[i])]);
+  }
+  for (let i = 0; i < rockLines.length - 1; i++) {
+    for (
+      let y = Math.min(rockLines[i + 1][0], rockLines[i][0]);
+      y <= Math.max(rockLines[i + 1][0], rockLines[i][0]);
+      y++
+    ) {
+      if (!cave[y]) {
+        cave[y] = [];
+      }
+      for (
+        let x = Math.min(rockLines[i + 1][1], rockLines[i][1]);
+        x <= Math.max(rockLines[i + 1][1], rockLines[i][1]);
+        x++
+      ) {
+        cave[y][x] = "#";
+        if (y > bottomRock) bottomRock = y;
+      }
+    }
+  }
+});
+
+let reachedAbyss = false;
+
+const sandDestination = (x, y) => {
+  if (x > bottomRock) {
+    reachedAbyss = true;
+    return;
+  }
+  if (!cave[x + 1]?.[y]) {
+    return sandDestination(x + 1, y);
+  } else if (!cave[x + 1]?.[y - 1]) {
+    return sandDestination(x + 1, y - 1);
+  } else if (!cave[x + 1]?.[y + 1]) {
+    return sandDestination(x + 1, y + 1);
+  }
+  return [x, y];
+};
+
+let sumOfSand = 0;
+while (!reachedAbyss) {
+  let sand = [0, 500];
+  let sandStop = sandDestination(sand[0], sand[1]);
+  if (sandStop) {
+    sumOfSand += 1;
+    if (!cave[sandStop[0]]) {
+      cave[sandStop[0]] = [];
+    }
+    cave[sandStop[0]][sandStop[1]] = "o";
+  }
+}
+console.log(sumOfSand);
